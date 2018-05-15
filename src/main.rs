7166bda4748923d0c01789e53350c67a85a9f15e
Benjamin Bouvier <public@benj.me>
2018-05-15 23:28:01 +0200

    cargo fmt + cargo clippy;

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use select::predicate::{Attr, Name};
 struct Entry {
     src: String,
     title: String,
-    hover: String
+    hover: String,
 }
 
 impl Entry {
@@ -18,32 +18,31 @@ impl Entry {
 
 fn main() {
     let base_url = String::from("https://xkcd.com");
-    let mut url: String = base_url.clone();
 
     let mut entries: Vec<Entry> = Vec::new();
 
+    let mut url: String = base_url.clone();
     for _ in 1..20 {
         let text = reqwest::get(url.as_str()).unwrap();
         let document = Document::from_read(text).unwrap();
 
         let comic = document.find(Attr("id", "comic")).next().unwrap();
-        let img = comic.find(Name("img")).next().unwrap();
 
+        let img = comic.find(Name("img")).next().unwrap();
+        // src has the following format: //img.xkcd.com/4242
         let src = String::from("https:") + img.attr("src").unwrap();
         let title = img.attr("alt").unwrap();
         let hover = img.attr("title").unwrap();
 
-        let e = Entry::new(src.to_string(), title.to_string(), hover.to_string());
+        let e = Entry::new(src.into(), title.into(), hover.into());
         entries.push(e);
 
         let prev = document.find(Attr("rel", "prev")).next().unwrap();
         let prev_link = prev.attr("href").unwrap();
-        let next_link = base_url.clone() + prev_link;
-
-        url = next_link;
+        url = base_url.clone() + prev_link;
     }
 
-    for entry in entries.iter() {
+    for entry in &entries {
         println!("{}: {}", entry.title, entry.hover);
         println!("src: {}", entry.src);
         println!();
